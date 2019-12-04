6d413ff2d42ba4a24b5ce8ef484d1e6ac98163b4
Shiva Venkatesh <shiva.venkatesh1@foodpanda.in>
2019-12-04 15:38:12 +0530

    binary search

---
+++ binary_search.js
@@ -0,0 +1,20 @@
+console.log('Binary search');
+
+let a = [1,3,5,7,9,11,13,15], s = 3;
+let i = 0, mid = Math.floor(a.length/2);
+
+while() {
+  mid = Math.floor((left + right)/2);
+  if(s<a[mid]) {
+    
+  } else if (s>a[mid]) {
+    
+  } else {
+    console.log("Found it!");
+    console.log('Required index is: ' + mid);
+    break;
+  }
+}
+
+
+
